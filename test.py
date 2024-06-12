0ac945af920aea740bbf052ee285e1d258d8648b
Myung-Il <ktr040415@gmail.com>
2024-06-12 22:06:25 +0900

    골드 2 - LIS 습득
    더 잘할 수 있었지만 머리가 너무 굳어서 중간까지 밖에서 못 구했다

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-a = 0
+n = int(input())
+cases = list(map(int, input().split()))
+lis = [0]
 
+for case in cases:
+    if lis[-1]<case:
+        lis.append(case)
+        break
+    
+    left = 0
+    right = len(lis)
 
-print(a)
+    while left<right:
+        mid = (right+left)//2
+        if lis[mid]<case:
+            left = mid+1
+        else:
+            right = mid
+    lis[right] = case
+
+print(len(lis)-1)
--- 단계별_풀기/27_이분탐색/7_12015_가장증가하는부분수열2.py
+++ 단계별_풀기/27_이분탐색/7_12015_가장증가하는부분수열2.py
@@ -5,15 +5,22 @@ n = int(input())
 l = list(map(int,input().split()))
 
 
-LIS = [-float("inf")]
-for idx in range(n):
-    left, right = 0, len(LIS)
-    while left<=right:
-        middle = (left+right)//2
-        if LIS[middle]<=l[idx]:left = middle+1
-        else:right = middle-1
+def BT(elm):
+    left, right = 0, len(LIS)    # 탐색 범위
+    while left<right:            # 탐색 범위를 넘어가면 멈춤
+        middle = (left+right)//2 # 중앙 위치
+        if LIS[middle]<elm:left = middle+1
+                            # 추가하려는 값이 탐색 값보다 크다면 왼쪽 포인터로 오른쪽으로 옮김
+        else:right = middle # 추가하려는 값이 탐색 값보다 작다면 오른쪽 포인터로 왼쪽으로 옮김
+                            # 만약 두 값이 같아진다면 elm가 들어갈 위치를 찾았다는 뜻이니 반복을 멈춘다
+    return right            # (left==middle+1)와 (right==middle)가 값은 값을 가르킴
 
-    if len(LIS)==middle:LIS.append(l[idx])
+LIS = [0]
+for elm in l: # 우리가 알고자 하는 증가 수열 요소를 하나씩 받음
+    if LIS[-1]<elm:LIS.append(elm) # 마지막 요소보다 크면 추가
+    else:LIS[BT(elm)] = elm        # 최소를 갱신해줌
+print(len(LIS)-1)
+
 
 f"""
 5
