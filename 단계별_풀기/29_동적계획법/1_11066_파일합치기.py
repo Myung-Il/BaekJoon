a6b5a78fda062b8125af5dd6f09fbf67c6bbb68e
Myung-Il <ktr040415@gmail.com>
2024-07-09 07:41:16 +0900

    골드 3 - 크누스 최적화
    일반 점화식보다 효율적이라고 함
    n**3을 n**2로 풀 수 있다고는 하는데 보는 방법을 터득해야 할 필요가 있음...

--- 단계별_풀기/29_동적계획법/1_11066_파일합치기.py
+++ 단계별_풀기/29_동적계획법/1_11066_파일합치기.py
@@ -14,6 +14,6 @@ for _ in range(int(int(input()))):
     for x in range(2, n):    # 1번째는 이미 구했기 때문에 2부터 함
         for y in range(n-x): # 빈칸과 이미 구한 것은 제외하고 구해야 하기 때문에 구한 것들은 제외함
             k[y][x+y] += min([k[y][idx]+k[idx+1][x+y] for idx in range(y, x+y)])
-        # 저장할 위치 += 최소값( 가로[idx를 기준으로 앞 값]+세로[뒷 값] idx를 1차원 위의 포인터)
+        # 저장할 위치 += 최소값( 가로[idx를 기준으로 앞 값]+세로[뒷 값] idx는 1차원 위의 포인터)
 
     print(k[0][-1])
--- 단계별_풀기/29_동적계획법/2_11049_행렬곱셈순서.py
+++ 단계별_풀기/29_동적계획법/2_11049_행렬곱셈순서.py
@@ -6,12 +6,17 @@ l = [list(map(int,input().split()))for _ in range(n)]
 
 k = [[0]*n for _ in range(n)]
 
-for idx in range(n-1):
+for idx in range(n-1): # 본인의 곱은 하지 않기때문에 1번째부터 함
     k[idx][idx+1] = l[idx][0]*l[idx][1]*l[idx+1][1]
+    # 행렬곱을 대각선에 구한 후 저장
 
-for x in range(2, n):
-    for y in range(n-x):
+for x in range(2, n):    # 1번째는 이미 했기에 2번째부터 함
+    for y in range(n-x): # 안 구한 자리를 찾아야 하기 때문에 이미 구한 만큼을 빼줌
         k[y][x+y] = min([k[y][idx]+k[idx+1][x+y] + l[y][0]*l[idx][1]*l[x+y][1] for idx in range(y, x+y)])
+        # 저장할 위치 = 최소값( 가로[idx를 기준으로 왼쪽 계산이 끝난 값]+세로[오른쪽 계산이 끝난 값]
+        #                      + 앞[현 위치 앞 수]*중앙[idx를 기준으로 곱 순서 바뀜]*뒤[현 위치의 뒷 수]
+        #                      idx는 1차원 위 )
+        #                      어차피 값은 곱하는 순서 때문에 가운데만 바뀜
 
 for i in k:print(i)
 
