36af5b9d4918278bc3c61715330e7bba67b61db4
Myung-Il <ktr040415@gmail.com>
2024-07-11 01:20:37 +0900

    2629번 추가 조사 코드
    나는 추의 값을 찾기위해서 각 무게당(1g)에서의 추의 무게를 저장하여 하나씩 대입하면서 찾았지만 이전의 값을 이용해서 더했을 때의 값과 현재 값과 뺏을 때의 값을 구하여 저장하고 그 자리에 1을 넣고 찾으려고 하는 구슬의 무게 자리에 1이 있다면 Y이고 0이면 N을 출력한다

--- test2.py
+++ test2.py
@@ -1,26 +1,31 @@
-import sys
-input = sys.stdin.readline
+'''
+1
+1, 0, 0, 1
+1, 1, 1, 1, 0, 1
+1, 1, 1, 1, 1, 1, 1, 0, 1
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1
 
-n = int(input())
-a = [list(map(int,input().split())) for i in range(n)]
-d = [[0 for i in range(n)] for i in range(n)]
+0  1  2  3  4  5  6  7  8  9 10
 
-# 인접한 2개의 행렬은 미리 곱하여 배열에 넣는다.
-for i in range(n-1):
-    d[i][i+1] = a[i][0]*a[i+1][0]*a[i+1][1]
+4
+2 3 3 3
+3
+1 4 10
 
-def dp(start,end):
-    if d[start][end] != 0:
-        return d[start][end]
-    if start == end:
-        return 0
-    
-    r = float('inf')
-    for i in range(start,end):
-        temp = dp(start,i) + dp(i+1,end) + a[start][0]*a[i+1][0]*a[end][1]
-        if r > temp:
-            r = temp
-    d[start][end] = r
-    return r
 
-print(dp(0,n-1))
+
+3
+1 2 4
+1
+5
+
+예상
+1
+1, 0, 0, 0, 1
+1, 1, 0, 1, 1, 1
+1, 1, 1, 1, 1, 1, 1, 1
+
+1, 0, 0, 0, 1
+1, 1, 0, 1, 1, 1
+1, 1, 1, 1, 1, 1, 1, 1
+'''
