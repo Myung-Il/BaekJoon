7d181194d1521976db625dbcea1ee771f28d9a0e
Myung-Il <ktr040415@gmail.com>
2024-07-31 15:50:38 +0900

    플레 5 - X
    어떠한 도움 없이 풀면 O
    그게 아니라면 X
    
    LIS에 관한 문제를 풀어본 적이 있으나 생각하지 못했고 결과 값을 도출하는 방법으로 index를 저장하고 역순으로 탐색하는 방법을 떠올리지 못했다, LIS를 순방향과 역방향으로 굴리고 2개를 조합하면 되지 않을까라고 생각했지 idx를 이용할 생각은 하지 못했다
    
    시간을 더 투자하고 더 넓고 다양하게 생각했다면 풀 수 있는 문제였다

--- test.py
+++ test.py
@@ -1,12 +1,31 @@
-l = [1, 2, 3]
+from sys import stdin
+input = lambda:stdin.readline().rstrip()
 
-def f(arr, result, idx, sum):
-    if len(arr)==idx:
-        result.append(sum)
-        return result
-    
-    f(arr, result, idx+1, sum)
-    f(arr, result, idx+1, sum+arr[idx])
-    return result
+def bt(e, dp):
+    left, right = 0, len(dp)
+    while left<right:
+        mid = (left+right)//2
+        if dp[0]>0 and dp[mid]>e:left=mid+1
+        elif dp[0]<0 and dp[mid]<e:left=mid+1
+        else:right=mid
+    return right
 
-print(sorted(f(l, [], 0, 0)))
+def solve(dp, num, A, B, C):
+    for idx in range(A, B, C):
+        if dp[-1]>a[idx]:
+            dp.append(a[idx])
+            num.append(idx)
+        else:
+            point = bt(a[idx], dp)
+            dp[point], num[point] = a[idx], idx
+    return dp, num
+
+
+n = int(input())
+a = list(map(int,input().split()))
+
+dp_f, f = solve([float("inf")], [0], n-1, -1, -1)
+dp_b, b = solve([-float("inf")], [0], 0, n, 1)
+
+print(dp_f[:0:-1], f[:0:-1])
+print(dp_b[1:], b[1:])
