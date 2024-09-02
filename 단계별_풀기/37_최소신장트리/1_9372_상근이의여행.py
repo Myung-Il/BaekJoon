e49427c1db7879566c26466cf9caab9060018b2b
Myung-Il <ktr040415@gmail.com>
2024-09-02 18:00:53 +0900

    실버 4 - O
    말 장난 문제
    이번 방학 동안의 비행 스케줄이 주어졌을 때, 상근이가 "가장 적은 종류"의 비행기를 타고 모든 국가들을 여행할 수 있도록 도와주자.

--- 단계별_풀기/37_최소신장트리/1_9372_상근이의여행.py
+++ 단계별_풀기/37_최소신장트리/1_9372_상근이의여행.py
@@ -1,4 +1,10 @@
-from sys import stdin, setrecursionlimit
-input = lambda:stdin.readline().rstrip()
-setrecursionlimit(10**6)
+for _ in range(int(input())):
+    n, m = map(int,input().split())
+    for _ in  range(m):input()
+    print(n-1)
 
+# 비행기를 몇번 타느냐가 아닌
+# 몇개의 비행기를 타느냐다
+# 다음 정점에 가기 위해서는 비행기를 타야한다
+# 한마디로 이미간 정점은 다시 갈 필요가 없다 어차피 돌아서 비행기를 탄다고 해도
+# 비행기도 아직 안 타본 비행기일 것이기 때문이다
