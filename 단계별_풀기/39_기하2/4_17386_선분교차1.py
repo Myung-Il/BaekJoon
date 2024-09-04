d90e260f18aca11b5ba1974539ccf823b74d8986
Myung-Il <ktr040415@gmail.com>
2024-09-05 04:21:18 +0900

    골드 3 - 2차
    1번 선분에서 x의 범위를 지정하고
    그 안에 두번째 선분의 교차점이 있는지 찾고 있는데 잘 안되는 중

--- 단계별_풀기/39_기하2/4_17386_선분교차1.py
+++ 단계별_풀기/39_기하2/4_17386_선분교차1.py
@@ -5,11 +5,10 @@ input=lambda:stdin.readline().rstrip()
 a1, b1, a2, b2 = map(int,input().split())
 a3, b3, a4, b4 = map(int,input().split())
 
-remainder1 = (a1-a2)*a1-(b1-b2)*b1
-remainder2 = (a3-a4)*a3-(b3-b4)*b3
+remainder = (a1-a2)*a1-(b1-b2)*b1
 
 for x in range(a1, a2+1):
-    if (b1-b2)*(a3-a4)*x - (b3-b4)*(a1-a2)*x - (remainder2*(a1-a2) - remainder1*(a3-a4)) == 0:
+    if (a1-a2)*b3<= (b1-b2)*x + remainder <=(a1-a2)*b4:
         print(1)
         break
 else:print(0)
