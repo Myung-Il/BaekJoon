72f05ce6cbac7c32bcb3593bd7bedb89186f8fe7
Myung-Il <ktr040415@gmail.com>
2024-09-05 23:25:19 +0900

    골드 2 - X
    정답을 봐도 이해하는데 꽤 시간이 걸렸다
    
    아쉽지도 않다

--- test.py
+++ test.py
@@ -1,46 +1,9 @@
-import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+def ccw(x1, y1, x2, y2, x3, y3):
+    return x1*y2+x2*y3+x3*y1 -x2*y1-x3*y2-x1*y3
 
-p = list(map(int, input().split()))
-v = [0 for _ in range(8)]
-pmt, res = [], []
+'''
+1 1 3 3
+2 3 1 4
 
-def dfs(idx):
-	if idx == 8:
-		pmt.append(res[:])
-		return
-	for next in range(8):
-		if v[next] == 0:
-			v[next] = 1
-			res.append(p[next])
-			dfs(idx+1)
-			res.pop()
-			v[next] = 0
-
-def ccw(a, b, c):
-	x1, y1 = 0, a
-	x2, y2 = ((b**2)/2)**0.5, ((b**2)/2)**0.5
-	x3, y3 = c, 0
-	l = (x1*y2+x2*y3+x3*y1)
-	r = (x2*y1+x3*y2+x1*y3)
-	temp = l-r
-	if temp <= 0:
-		return True
-	return False
-		
-def checker(pmt):
-	cnt = 0
-	for idx in range(len(pmt)):
-		flag = True
-		for i in range(8):
-			if not ccw(pmt[idx][i], pmt[idx][(i+1)%8], pmt[idx][(i+2)%8]):
-				flag = False
-				break
-		if flag:
-			cnt += 1
-	return cnt
-
-
-dfs(0)
-print(checker(pmt))
+'''
+print(ccw(1, 1, 3, 3, 1, 3))
--- 단계별_풀기/39_기하2/5_17387_선분교차2.py
+++ 단계별_풀기/39_기하2/5_17387_선분교차2.py
@@ -1,24 +1,30 @@
 from sys import stdin
 input=lambda:stdin.readline().rstrip()
 
+def solve():
+    if a*b == 0 and c*d == 0: # 평행할 때
+        if mx1 <= mx4 and mx3 <= mx2 and my1 <= my4 and my3 <= my2:
+            return 1 # 두 점 중 하나의 x값과 y값은 선분 L1 범위안에 있어야 한다
+    else:
+        if a*b <= 0 and c*d <= 0:
+            return 1
+
+    return 0
 
 def ccw(x1, y1, x2, y2, x3, y3):
     return x1*y2+x2*y3+x3*y1 -x2*y1-x3*y2-x1*y3
 
 
-a1, b1, a2, b2 = map(int,input().split())
-a3, b3, a4, b4 = map(int,input().split())
+a1, b1, a2, b2 = map(int,input().split()) # 선분 L1 위의 두 점
+a3, b3, a4, b4 = map(int,input().split()) # 선분 L2 위의 두 점
 
+# 두 선분이 평행할 때, 교차하는지 알기 위한 각 x,y에서 최소와 최대를 구한다
 mx1, my1, mx2, my2 = min(a1, a2), min(b1, b2), max(a1, a2), max(b1, b2)
 mx3, my3, mx4, my4 = min(a3, a4), min(b3, b4), max(a3, a4), max(b3, b4)
 
-a = ccw(a1, b1, a2, b2, a3, b3)
-b = ccw(a1, b1, a2, b2, a4, b4)
-c = ccw(a3, b3, a4, b4, a1, b1)
-d = ccw(a3, b3, a4, b4, a2, b2)
+a = ccw(a1, b1, a2, b2, a3, b3) # 선분 L1에 대해 점3이 어디에 있는지 확인
+b = ccw(a1, b1, a2, b2, a4, b4) # 선분 L1에 대해 점4가 어디에 있는지 확인
+c = ccw(a3, b3, a4, b4, a1, b1) # 선분 L2에 대해 점1이 어디에 있는지 확인
+d = ccw(a3, b3, a4, b4, a2, b2) # 선분 L2에 대해 점2가 어디에 있는지 확인
 
-if a*b==0 and c*d==0:
-    if mx1 <= mx4 and mx3 <= mx2 and my1 <= my4 and my3 <= my2:
-        print(1);exit()
-elif a*b<=0 and c*d<=0:print(1);exit()
-else:                  print(0)
+print(solve())
