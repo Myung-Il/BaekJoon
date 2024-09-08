e7c5dd5d9e6b3e5a92d59020e5970b550d55b12f
Myung-Il <ktr040415@gmail.com>
2024-09-08 20:38:15 +0900

    플레 5 - 5차
    유니온 파인드에 문제가 있다는 말은 있지만 정확히 왜 문제인지는 다들 몰라하는 것 같다

--- 단계별_풀기/39_기하2/7_2162_선분그룹.py
+++ 단계별_풀기/39_기하2/7_2162_선분그룹.py
@@ -2,10 +2,9 @@ from sys import stdin
 input=lambda:stdin.readline().rstrip()
 
 def solve():
-    parents = [-1]*n
     parents[0] = 0
-    for idx1 in range(n-1):
-        for idx2 in range(idx1+1, n):
+    for idx1 in range(n):
+        for idx2 in range(n):
             if parents[idx1]==parents[idx2]:continue
             a1, b1, a2, b2 = point[idx1][0][0], point[idx1][0][1], point[idx1][1][0], point[idx1][1][1]
             a3, b3, a4, b4 = point[idx2][0][0], point[idx2][0][1], point[idx2][1][0], point[idx2][1][1]
@@ -19,32 +18,36 @@ def solve():
             mx3, my3, mx4, my4 = min(a3, a4), min(b3, b4), max(a3, a4), max(b3, b4)
 
             if a*b == 0 and c*d == 0:
-                if mx1 <= mx4 and mx3 <= mx2 and my1 <= my4 and my3 <= my2:
-                    if parents[idx1]==-1:parents[idx1] = parents[idx2]
-                    else:                parents[idx2] = parents[idx1]
-            elif a*b <= 0 and c*d <= 0:
-                    if parents[idx1]==-1:parents[idx1] = parents[idx2]
-                    else:                parents[idx2] = parents[idx1]
+                if mx1 <= mx4 and mx3 <= mx2 and my1 <= my4 and my3 <= my2:union(idx1, idx2)
+            elif a*b <= 0 and c*d <= 0:                                    union(idx1, idx2)
     return parents
 
 def ccw(x1, y1, x2, y2, x3, y3):
     return x1*y2+x2*y3+x3*y1 -x2*y1-x3*y2-x1*y3
 
+def find(e):
+    if e!=parents[e]:
+        parents[e] = find(parents[e])
+    return parents[e]
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x<y:parents[y] = x
+    else:  parents[x] = y
+
 
 n = int(input())
 point = [0]*n
+parents = [i for i in range(n)]
 for idx in range(n):
     a1, b1, a2, b2 = map(int, input().split())
     point[idx] = [[a1, b1], [a2, b2]]
 
 result = solve()
-l = [0]*(n+1)
+d = {}
 for elm in result:
-    l[elm] += 1
-
-s, mx = 0, max(l[:n])
-for idx in range(n):
-    if l[idx]:s+=1
-print(s+l[-1])
-print(mx if mx else 1)
-print(result)
+    if not d.get(elm):d[elm] = 0
+    d[elm] += 1
+print(len(d))
+print(max(d.values()))
