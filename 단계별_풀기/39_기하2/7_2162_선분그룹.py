466c04243b5b5f4cab0f8a0b18a7d8ed263fbf21
Myung-Il <ktr040415@gmail.com>
2024-09-08 20:47:49 +0900

    플레 5 - O
    마지막에 서로 같은 그룹안에 있는지 확인 하지 않아서 계속 틀린거였다
    
    출력은 당연히 계속 틀렸고 유니온파인드도 틀렸었다
    
    물론 재탐색 차이가 있긴 했다
    다른 사람들은 재탐색을 해주지 않은것 같다

--- 단계별_풀기/39_기하2/7_2162_선분그룹.py
+++ 단계별_풀기/39_기하2/7_2162_선분그룹.py
@@ -2,10 +2,9 @@ from sys import stdin
 input=lambda:stdin.readline().rstrip()
 
 def solve():
-    parents[0] = 0
-    for idx1 in range(n):
-        for idx2 in range(n):
-            if parents[idx1]==parents[idx2]:continue
+    for idx1 in range(n):     # 비교를 할
+        for idx2 in range(n): # 두 선분
+            if parents[idx1]==parents[idx2]:continue # 두 선분 사이에 교점이 있다면 지나친다
             a1, b1, a2, b2 = point[idx1][0][0], point[idx1][0][1], point[idx1][1][0], point[idx1][1][1]
             a3, b3, a4, b4 = point[idx2][0][0], point[idx2][0][1], point[idx2][1][0], point[idx2][1][1]
             
@@ -18,8 +17,8 @@ def solve():
             mx3, my3, mx4, my4 = min(a3, a4), min(b3, b4), max(a3, a4), max(b3, b4)
 
             if a*b == 0 and c*d == 0:
-                if mx1 <= mx4 and mx3 <= mx2 and my1 <= my4 and my3 <= my2:union(idx1, idx2)
-            elif a*b <= 0 and c*d <= 0:                                    union(idx1, idx2)
+                if mx1 <= mx4 and mx3 <= mx2 and my1 <= my4 and my3 <= my2:union(idx1, idx2) # 교점이 존재한다면 두 선분이
+            elif a*b <= 0 and c*d <= 0:                                    union(idx1, idx2) # 같은 집합에 있도록 설정해준다
     return parents
 
 def ccw(x1, y1, x2, y2, x3, y3):
@@ -44,7 +43,8 @@ for idx in range(n):
     a1, b1, a2, b2 = map(int, input().split())
     point[idx] = [[a1, b1], [a2, b2]]
 
-result = solve()
+result = solve()                    # 집합을 설정했는데 마지막에 집합 3개를 합칠 수 도 있다
+result = [find(i)for i in range(n)] # 그러니 집합이 서로 같은 것인지 확인해준다
 d = {}
 for elm in result:
     if not d.get(elm):d[elm] = 0
