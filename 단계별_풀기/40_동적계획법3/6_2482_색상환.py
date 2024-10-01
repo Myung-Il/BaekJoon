b019a92c50d3c6cb09eb47925ff6190a0b1b96ed
Myung-Il <ktr040415@gmail.com>
2024-10-02 00:47:23 +0900

    골드 3 - O
    문제를 잘 못 이해해서 답을 보고도 왜 이게 맞는거지 생각하고 있다가 다른 사람의 풀이를 과정을 보고 문제 자체를 잘 못 이해하고 있구나 하고 다시 문제를 풀어보았다
    
    test가 다른 사람의 풀이다
    
    문제를 다시 이해하고 다른 사람의 풀이 과정을 봤는데 굳이 저렇게 할 필요가 있나? 라는 생각이 들게 되었다
    
    생략할 수 있는 부분은 다 줄였다
    
    점화식
    
    0  1  2  3
    1
    2
    3
    4  2
    5  5
    6  9  2
    7 14  7
    8 20 16  2
    
    idx 0번째는 이미 구했으니 1부터 구해준다
    idx 1번째는 색을 2개 구하는 법이다
    
    5/2는 5가지 경우를 가진다
    색 하나를 쪼개서 새로운 색을 만들면 6/2는 5/2의 5가지 경우를 가면서 새로운 경우를 가진다
    6/2 = 5/2 + A라는 식이 나온다
    
    이때 A는 4/1의 갯수와 같다
    
    칸이 새로 생겼는데 현재 이 칸은 5/2에서 칠하지 않았다, 왜?
    바로 방금 생겼기 때문에 해당을 채워주는 조건들이 필요하다
    해당 칸과 양옆을 제외한 나머지 3개와 마주본다, 그렇다면 값은 3개가 된다
    
    6/2 = 9
    5/2 = 5
    4/1 = 4, 1개는 어디로 갔는가
    
    칸을 쪼개 주면서 새로운게 생겼다, 그렇다면 기존에 붙어 있던 양옆의 색은 서로 마주볼 수 있게 된다
    새로 생긴게 양 옆을 건들면 안되는거지 각각 서로는 건드려도 된다 이렇게 새로 1개가 추가 된다

--- 단계별_풀기/40_동적계획법3/6_2482_색상환.py
+++ 단계별_풀기/40_동적계획법3/6_2482_색상환.py
@@ -6,6 +6,7 @@ n = int(input())
 k = int(input())
 dp = [[0]*k for _ in range(n)]
 
+
 for ni in range(n):
     dp[ni][0] = ni+1 # 색을 1개만 선택할 때는 색의 갯수만큼 선택 할 수 있다
     if ni<3:continue # 3개 이상부터 2개이상으로 씩 선택할 수 있다
