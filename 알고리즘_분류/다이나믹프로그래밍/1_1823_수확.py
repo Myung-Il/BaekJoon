20a3c466495c18d651a85623c29871ff5d2685d6
Myung-Il <ktr040415@gmail.com>
2025-03-21 16:34:38 +0900

    골드 3 - X
    Top-down방식을 이용하고 메모리제이션을 이용해서 내용을 기록한다

--- 알고리즘_분류/다이나믹프로그래밍/1_1823_수확.py
+++ 알고리즘_분류/다이나믹프로그래밍/1_1823_수확.py
@@ -3,36 +3,19 @@ input = lambda:stdin.readline().strip()
 setrecursionlimit(10**6)
 
 n = int(input())
-farm = [int(input())for _ in range(n)]
-memory = {
-    day:{
-        start:{
-            end:None
-            for end in range(n)}
-            for start in range(n)}
-            for day in range(1, n+1)}
+farm = [0] + [int(input())for _ in range(n)]
 
-def rice(day, start, end):
-    if start==end:return day*farm[start]
-    if memory[day][start][end]:return memory[day][start][end]
+dp = [[0]*(n+1) for _ in range(n+1)]
+
+def rice(day, start, end):                         # 처음에는 Top-down으로 내려간다
+    if start>end:return 0                          # 찾는 값이 아니면 0
+    if dp[start][end]:return dp[start][end]        # 이미 찾은 값이면 반환
+
+    dp[start][end] = max(                          # 최대 찾기
+        day*farm[start]+rice(day+1, start+1, end), # 왼쪽부터 계산했을 때
+        day*farm[ end ]+rice(day+1, start, end-1)  # 오른쪽부터 계산했을 때
+        )
     
-    left = rice(day+1, start+1, end)
-    right = rice(day+1, start, end-1)
+    return dp[start][end]
 
-    if left > right:
-        op = left+day*farm[start]
-        memory[day][start][end] = op
-        return op
-    else:
-        op = right+day*farm[end]
-        memory[day][start][end] = op
-        return op
-
-print(rice(1, 0, n-1))
-
-'''
-1 3 1 5 2
-
-
-[ 1  ]
-'''
+print(rice(1, 1, n))
