b2766a6508cfa0492ae2bcece03ec3212e29dfad
Myung-Il <ktr040415@gmail.com>
2025-03-21 15:06:58 +0900

    골드 3 - 2차
    중복되는 경우를 어떻게 줄이면 좋을지 모르겠다

--- 알고리즘_분류/다이나믹프로그래밍/1_1823_수확.py
+++ 알고리즘_분류/다이나믹프로그래밍/1_1823_수확.py
@@ -1,14 +1,17 @@
-from sys import stdin
-from collections import deque as dq
+from sys import stdin, setrecursionlimit
 input = lambda:stdin.readline().strip()
+setrecursionlimit(10**6)
 
 n = int(input())
-farm = dq([int(input())for _ in range(n)])
+farm = [int(input())for _ in range(n)]
 
-result = 0
-for day in range(1, n+1):
-    if farm[0] < farm[-1]:
-        result += day*farm.popleft()
-    else:result += day*farm.pop()
+def rice(day, start, end):
+    if start==end:return day*farm[start]
+    
+    left = rice(day+1, start+1, end)
+    right = rice(day+1, start, end-1)
 
-print(result)
+    if left > right:return left +day*farm[start]
+    else:           return right+day*farm[end]
+
+print(rice(1, 0, n-1))
