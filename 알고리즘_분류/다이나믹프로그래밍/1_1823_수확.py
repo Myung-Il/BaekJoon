90b70500f862d5a0e68cb6c3734c7a78a5d2c314
Myung-Il <ktr040415@gmail.com>
2025-03-21 15:18:53 +0900

    골드 3 - 3차
    메모리제이션 기법을 이용했으나
    2000**3 크기의 딕셔너리로 인해서 메모리 초과가 떴다

--- 알고리즘_분류/다이나믹프로그래밍/1_1823_수확.py
+++ 알고리즘_분류/다이나믹프로그래밍/1_1823_수확.py
@@ -4,14 +4,35 @@ setrecursionlimit(10**6)
 
 n = int(input())
 farm = [int(input())for _ in range(n)]
+memory = {
+    day:{
+        start:{
+            end:None
+            for end in range(n)}
+            for start in range(n)}
+            for day in range(1, n+1)}
 
 def rice(day, start, end):
     if start==end:return day*farm[start]
+    if memory[day][start][end]:return memory[day][start][end]
     
     left = rice(day+1, start+1, end)
     right = rice(day+1, start, end-1)
 
-    if left > right:return left +day*farm[start]
-    else:           return right+day*farm[end]
+    if left > right:
+        op = left+day*farm[start]
+        memory[day][start][end] = op
+        return op
+    else:
+        op = right+day*farm[end]
+        memory[day][start][end] = op
+        return op
 
 print(rice(1, 0, n-1))
+
+'''
+1 3 1 5 2
+
+
+[ 1  ]
+'''
