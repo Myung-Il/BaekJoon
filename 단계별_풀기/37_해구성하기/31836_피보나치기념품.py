e7b2a7fb52424391f30194ca50e03f675f086317
김태랑 <92520262+Myung-Il@users.noreply.github.com>
2025-04-08 17:00:36 +0900

    실버1 - 2차
    아직도 문제가 생긴다!
    뭐가 문제인지 모르겠으니 더 알아봐야겠다

--- 단계별_풀기/37_해구성하기/31836_피보나치기념품.py
+++ 단계별_풀기/37_해구성하기/31836_피보나치기념품.py
@@ -1,13 +1,16 @@
 n = int(input())
 
-l = [num for num in range(n+1)]
+if n==2:
+    print('''1
+1
+1
+2''')
+    exit()
 
-l1, l2 = [], []
-for idx in range(3, n+1, 3):
-    l1.append(l[idx])
-    l2.append(l[idx-1])
-    l2.append(l[idx-2])
+l2 = [num for num in range(1, n+1)if not num%3]
+l1 = [num for num in range(1, n+1)if num%3 and num<l2[-1]]
 
-print(l)
-print(l1)
-print(l2)
+print(len(l1))
+print(*l1)
+print(len(l2))
+print(*l2)
