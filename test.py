63f5154989b53922d3f8b5d56faa31891079d83b
김태랑 <92520262+Myung-Il@users.noreply.github.com>
2025-04-08 17:16:25 +0900

    세이브

--- test.py
+++
@@ -1,3 +0,0 @@
-for num in range(1, 2+1):
-    if not num%3:print(123)
-    print(num)
--- 단계별_풀기/37_해구성하기/31836_피보나치기념품.py
+++ 단계별_풀기/37_해구성하기/31836_피보나치기념품.py
@@ -1,16 +1,7 @@
 n = int(input())
 
-if n==2:
-    print('''1
-1
-1
-2''')
-    exit()
+fibo = [0, 1, 1]
+for idx in range(2, n):
+    fibo.append(fibo[idx]+fibo[idx-1])
 
-l2 = [num for num in range(1, n+1)if not num%3]
-l1 = [num for num in range(1, n+1)if num%3 and num<l2[-1]]
-
-print(len(l1))
-print(*l1)
-print(len(l2))
-print(*l2)
+    print(fibo)
