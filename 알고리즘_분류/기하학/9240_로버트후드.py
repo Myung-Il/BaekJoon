d6d1ceac682b7ec6a7a324bc331a376ef80375c8
김태랑 <92520262+Myung-Il@users.noreply.github.com>
2025-04-16 10:16:52 +0900

    플레3 - 8차

--- 알고리즘_분류/기하학/9240_로버트후드.py
+++ 알고리즘_분류/기하학/9240_로버트후드.py
@@ -1,12 +1,11 @@
 from sys import stdin
-import math
 input=lambda:stdin.readline().rstrip()
 
 def ccw(x1, y1, x2, y2, x3, y3):
     return x1*y2+x2*y3+x3*y1 -x2*y1-x3*y2-x1*y3
 
 def length(x1, y1, x2, y2):
-    return math.sqrt((x1-x2)**2+(y1-y2)**2)
+    return ((x1-x2)**2+(y1-y2)**2)**0.5
 
 
 def monotoneChain():
@@ -25,28 +24,28 @@ def monotoneChain():
     return lower, upper
 
 def rotatingCalipers(lower, upper):
-    mx = 0
     stack = lower+upper
     size = len(stack)
 
     li, ui = 0, len(lower)
+    mx = length(*stack[li], *stack[ui])
     for _ in range(size):
-        a, b = stack[li%size], stack[(li+1)%size]
-        c, d = stack[ui%size], stack[(ui+1)%size]
+        a, b = stack[li], stack[(li+1)%size]
+        c, d = stack[ui], stack[(ui+1)%size]
         
         point1 = b[0]-a[0], b[1]-a[1]
         point2 = d[0]-c[0], d[1]-c[1]
         
-        if ccw(*point1, 0, 0, *point2): li+=1
-        else: ui+=1
+        if ccw(*point1, 0, 0, *point2): li = (li+1)%size
+        else: ui = (ui+1)%size
         
-        mx = max(mx, length(*a, *c))
+        mx = max(mx, length(*stack[li], *stack[ui]))
     return mx
         
 
 n = int(input())
 points = [tuple(map(int,input().split()))for _ in range(n)]
-points.sort(key=lambda pos:(pos[1], pos[0]))
+points.sort()
 
 con1, con2 = monotoneChain()
 rot = rotatingCalipers(con1, con2)
