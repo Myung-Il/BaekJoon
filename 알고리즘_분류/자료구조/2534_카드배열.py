5a7f8426c3123ac92105f8152dc6c543d2833b26
김태랑 <92520262+Myung-Il@users.noreply.github.com>
2025-04-19 23:49:24 +0900

    다이아5 - 3차
    시간 초과

--- 알고리즘_분류/자료구조/2534_카드배열.py
+++ 알고리즘_분류/자료구조/2534_카드배열.py
@@ -1,47 +1,41 @@
 from sys import stdin
-import heapq as hq
 from collections import deque
 input = lambda:stdin.readline().rstrip()
 
-def topologySort(type, range):
+def topologySort(load, bool, diff=0):
     # 위상 정렬렬
     indegree = [0]*(k+1) # 부모 수
     graph = dict()       # 진입
 
+    load = sorted(load, reverse=bool)
     for a, b in load:
-        if graph.get(a):hq.heappush(graph[a], b*type)
-        else:
-            graph[a] = []
-            hq.heappush(graph[a], b*type)
+        if graph.get(a):graph[a].append(b)
+        else: graph[a] = [b]
         indegree[b] += 1
 
-    result = set()
+    result = 0
+    order = k-1
     queue = deque() # 큐
-    for idx in range:
-        if not indegree[idx] and idx not in result:
+    for idx in sorted(range(0, k), reverse=bool):
+        if not indegree[idx]:
             queue.append(idx)
             while queue:
                 x = queue.popleft() # 현재 위치
-                result.add(x)
+                result += (x+diff)*n**order
+                order -= 1
                 if not graph.get(x): continue
                 for g in graph[x]:
-                    node = g*type
+                    node = g
                     indegree[node] -= 1    # 차수 감소
                     if indegree[node]==0:  # 더 이상 연결된 부모가 없다면,
                         queue.append(node) # 스스로 들고 일어남
-    return result
-
-def solve(group, diff=0):
-    result = 0
-    order = k-1
-    for elm in group:
-        result += (elm+diff)*n**order
-        order -= 1
+                        indegree[node] -= 1
+                
     return result
 
 n, k, p = map(int, input().split())
-load = [list(map(int, input().split()))for _ in range(p)]
+load = [tuple(map(int, input().split()))for _ in range(p)]
 
-mn = solve(topologySort(1, range(0, k)))
-mx = solve(topologySort(-1, range(k-1, -1, -1)), n-k)
+mn = topologySort(load, False)
+mx = topologySort(load, True, n-k)
 print(mx-mn)
