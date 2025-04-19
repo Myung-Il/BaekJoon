a02d5f494699232e686242381bd3fa7809fa9fa2
김태랑 <92520262+Myung-Il@users.noreply.github.com>
2025-04-20 02:09:23 +0900

    다이아5 - 8차

--- test2.py
+++ test2.py
@@ -1,5 +1,1 @@
-if []:print(0)
-else:print(1)
-
-if [0]:print(0)
-else:print(1)
+print(1<<5-1)
--- 알고리즘_분류/자료구조/2534_카드배열.py
+++ 알고리즘_분류/자료구조/2534_카드배열.py
@@ -2,6 +2,15 @@ from sys import stdin
 import heapq as hq
 input = lambda:stdin.readline().rstrip()
 
+def fs(a, order, mod=10**9+7):    # a^order mod
+    y = 1
+    while order > 0:
+        if order & 1  == 1:
+            y = (a * y) % mod
+        a = (a * a) % mod
+        order = order >> 1
+    return y
+
 def topologySort(load):
     indegree = 1<<(k+1)
     graph = [[]for _ in range(k+1)]
@@ -12,42 +21,21 @@ def topologySort(load):
         graph[a].append(b)
         indegree = indegree|1<<b
 
-    result = []
     queue = [] # 큐
     for idx in range(0, k):
         if not indegree&1<<idx:
             hq.heappush(queue, idx)
 
+    result, order = 0, k-1
     while queue:
         x = hq.heappop(queue) # 현재 위치
-        result.append(x)
+        result += (n-k)*fs(n, order)
+        order -= 1
         if not graph[x]: continue
         for g in graph[x]:hq.heappush(queue, g)
-    return result
-
-def fs(a, x, n):    # a^x mod n
-    y = 1
-    while x > 0:
-        if x & 1  == 1:         # 지수의 LSB가 1인지 확인
-            y = (a * y) % n     # Multiply Operation
-        a = (a * a) % n         # Square Operation
-        x = x >> 1
-    return y
-
-def solve(group, diff=0, mod=10**9+7):
-    result = 0
-    order = k-1
-    for elm in group:
-        result += (elm+diff)*fs(n, order, mod)
-        order -= 1
     return result
 
 
 n, k, p = map(int, input().split())
 load = [tuple(map(int, input().split()))for _ in range(p)]
-group = topologySort(load)
-
-mn = solve(group)
-mx = solve(group, n-k)
-
-print(mx-mn)
+print(topologySort(load))
