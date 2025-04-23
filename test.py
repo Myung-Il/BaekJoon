02d3d7ed973e1d267642399cc21ddf82969b5720
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2025-04-23 11:26:27 +0900

    [save]
    입력 이상함

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-def height(x1, y1, x2, y2, px, py):
-    ax, ay = px - x1, py - y1
-    bx, by = x2 - x1, y2 - y1
-
-    cross = abs(ax*by - ay*bx)
-    base = ((bx)**2 + (by)**2)**0.5
-    return cross/base
-
-
-a = 0, 0
-b = 1, 1
-print(height(*a, *b, 0, 2))
+print(6%5)
---
+++ 알고리즘_분류/기하학/2699_격자점컨벡스헐.py
@@ -0,0 +1,36 @@
+from sys import stdin
+input = lambda:stdin.readline().rstrip()
+
+def ccw(x1, y1, x2, y2, x3, y3):
+    return x1*y2+x2*y3+x3*y1 -x2*y1-x3*y2-x1*y3
+
+
+def monotoneChain():
+    right, left = [], []
+    
+    for point in points:
+        while len(left)>1 and ccw(*left[-2], *left[-1], *point) >= 0:left.pop()
+        left.append(point)
+    left.pop()
+    
+    for point in reversed(points):
+        while len(right)>1 and ccw(*right[-2], *right[-1], *point) >= 0:right.pop()
+        right.append(point)
+    right.pop()
+
+    return left+right
+
+
+for _ in range(int(input())):
+    n = int(input())
+    points = []
+    for _ in range(n//5+n%5):
+        sub = list(map(int, input().split()))
+        for idx in range(0, len(sub)-1, 2):
+            points.append((sub[idx], sub[idx+1]))
+    points.sort(key=lambda p:(-p[1], p[0]))
+
+    stack = monotoneChain()
+    print(len(stack))
+    for x, y in stack:
+        print(x, y)
