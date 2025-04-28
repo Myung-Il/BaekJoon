ed25c4581d3ada5f21151e7f0f90407551c3bea6
김태랑 <92520262+Myung-Il@users.noreply.github.com>
2025-04-28 12:05:37 +0900

    [update]

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print(6%5)
+def verticality(x1, y1, x2, y2, x3, y3):
+    numerator = abs((y2-y1)*x3 - (x2-x1)*y3 + x2*y1 - y2*x1)
+    denominator = ((y2-y1)**2 + (x2-x1)**2)**0.5
+    return numerator / denominator
+
+def horizontal(x1, y1, x2, y2, x3, y3):
+    t = ((x3-x1)*(x2-x1) + (y3-y1)*(y2-y1)) / ((x2-x1)**2 + (y2-y1)**2)
+    return (x1 + t*(x2-x1), y1 + t*(y2-y1))
+
+def horizontal1(x1, y1, xD, yD):
+    return ((xD-x1)**2 + (yD-y1)**2)**0.5
+
+# 예시 사용법
+A = (0, 0)
+B = (1, 0)
+C = (-2, -1)
+
+# 직선 AB에 수직인 점 C에서 D까지의 최단 거리
+D = horizontal(*A, *B, *C)
+distance = horizontal1(*A, *D)
+print(f"점 A에서 점 D까지의 거리: {distance}")
--- 알고리즘_분류/기하학/19586_울타리.py
+++ 알고리즘_분류/기하학/19586_울타리.py
@@ -7,13 +7,14 @@ def ccw(x1, y1, x2, y2, x3, y3):
 def distance(x1, y1, x2, y2):
     return ((x1-x2)**2+(y1-y2)**2)**0.5
 
-def height(x1, y1, x2, y2, x3, y3):
-    px1, py1 = x1-x2, y1-y2
-    px2, py2 = x3-x2, y3-y2
+def verticality(x1, y1, x2, y2, x3, y3):
+    numerator = abs((y2-y1)*x3 - (x2-x1)*y3 + x2*y1 - y2*x1)
+    denominator = ((y2-y1)**2 + (x2-x1)**2)**0.5
+    return numerator / denominator
 
-    cross = abs(px1*py2 - py1*px2)
-    base_len = (px2**2 + py2**2) ** 0.5
-    return cross / base_len
+def horizontal(x1, y1, x2, y2, x3, y3):
+    t = ((x3-x1)*(x2-x1) + (y3-y1)*(y2-y1)) / ((x2-x1)**2 + (y2-y1)**2)
+    return (x1 + t*(x2-x1), y1 + t*(y2-y1))
 
 
 def monotoneChain():
@@ -24,20 +25,20 @@ def monotoneChain():
         lower.append(point)
     lower.pop()
     
-    for point in reversed(points):
+    for point in points[::-1]:
         while len(upper)>1 and ccw(*upper[-2], *upper[-1], *point) <= 0:upper.pop()
         upper.append(point)
     upper.pop()
     
-    return lower, upper
+    return lower+upper, len(lower)
 
-def rotatingCalipers(lower, upper):
-    stack = lower+upper
+def rotatingCalipers(stack, num):
     size = len(stack)
     
-    li, ui = 0, len(lower)
-    l = [0]*size
-
+    li, ui = 0, num
+    squarepoints = [[0, 0, 0]for _ in range(size)]
+    squarepoints[li][1] = stack[ui]
+    
     cnt = 0
     while cnt!=size:
         a, b = stack[li], stack[(li+1)%size]
@@ -48,25 +49,44 @@ def rotatingCalipers(lower, upper):
 
         if ccw(*point1, 0, 0, *point2)>0:
             li = (li+1)%size
-            cnt += 1
-        else: ui = (ui+1)%size
-        l[li] = [li, ui]
-    return l
+            cnt+=1
+            squarepoints[li][1] = c
+        else:
+            ui = (ui+1)%size
+            squarepoints[li][1] = d
+            
+    for li in range(size):
+        a, b = stack[li], stack[(li+1)%size]
+        squarepoints[li][2] = distance(*a, *b)
+        
+        ui = (li+2)%size
+        while (c:=stack[ui])!=squarepoints[li][1]:
+            ui = (ui+1)%size
+            p = horizontal(*a, *b, *c)
+            squarepoints[li][2] = max(squarepoints[li][2], distance(*a, *p))
+        
+        ui = (li-1)%size
+        while (c:=stack[ui])!=squarepoints[li][1]:
+            ui = (ui-1)%size
+            p = horizontal(*a, *b, *c)
+            squarepoints[li][0] = max(squarepoints[li][0], distance(*a, *p))
+        
+        squarepoints[li][1] = verticality(*a, *b, *squarepoints[li][1])
+            
+    return squarepoints
 
-def solve(stack, under):
-    result = 0
-    size = len(stack)
-
-    for u, t in under:...
-
+def solve(square):
+    mx = 0
+    for left, col, right in square:
+        row = left+right
+        mx = max(mx, (row+col)*2)
 
 
 n = int(input())
-points = [tuple(map(int,input().split()))for _ in range(n)]
+points = [list(map(int, input().split()))for _ in range(n)]
 points.sort()
 
-con1, con2 = monotoneChain()
-rot = rotatingCalipers(con1, con2)
-stack = con1+con2
-
-print(solve(stack, rot))
+stack, cnt = monotoneChain()
+square = rotatingCalipers(stack, cnt)
+print(square)
+print(solve(square))
