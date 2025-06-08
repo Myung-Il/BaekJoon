ff7d5384d0cff065db2ebcced6073fea088d9516
Myung-Il <ktr040415@gmail.com>
2025-06-09 00:04:45 +0900

    골드3 - 1차
    키 에러
    새로 만들기 싫어서 AUP를 이용해서 받고 있는데 정보 갱신이 이상하게 된 것 같다

--- 알고리즘_분류/최대유량/6086_최대유량.py
+++ 알고리즘_분류/최대유량/6086_최대유량.py
@@ -1,24 +1,61 @@
 from sys import stdin
+from collections import defaultdict, deque
+from string import ascii_uppercase
+
 input = lambda:stdin.readline().rstrip()
-def multi():
-    s1, s2, i = input().split()
-    return s1, s2, int(i)
+AUP = {ascii:False for ascii in ascii_uppercase}
+INF = float("inf")
 
+class EdmondsKarp:
+    def __init__(self, G):
+        self.graph = G
 
-class Pipe:
-    def __init__(self, source):
-        self.source = source
-        self.sinks = dict()
+    def BFS(self, source, sink, parent):
+        visit = AUP
+        queue = deque()
+
+        visit[source] = True
+        queue.append(source)
+
+        while queue:
+            vertex = queue.popleft()
+
+            for key in self.graph[vertex]:
+                if not visit[key] and self.graph[vertex][key]>0:
+                    visit[key] = True
+                    queue.append(key)
+                    parent[key] = vertex
+                    if key==sink:return True
+        return False
+
     
-    def sinkAdd(self, sink, capacity):self.sinks[sink] = capacity
+    def maxFlow(self, source, sink):
+        parent = AUP
+        flow = 0
 
+        while self.BFS(source, sink, parent):
+            path = INF
+            sk = sink
+            while sk!=source:
+                path = min(path, self.graph[parent[sk]][sk])
+                sk = parent[sk]
+        
+            flow += path
+        
+            sk = sink
+            while sk!=source:
+                vertex = parent[sk]
+                self.graph[vertex][sink] -= path
+                self.graph[sink][vertex] += path
+                sk = parent[vertex]
+        return flow
 
 n = int(input())
-pipes = dict()
-
+graph = {ascii2:{ascii1:0 for ascii1 in ascii_uppercase} for ascii2 in ascii_uppercase}
 for _ in range(n):
-    source, sink, capacity = multi()
-    if pipes.get(source):pipes[source].append(capacity)
-    else:
-        pipes[source] = Pipe(source)
-    pipes[source].sinkAdd(sink, capacity)
+    source, sink, capacity = input().split()
+    graph[source][sink] = int(capacity)
+
+EK = EdmondsKarp(graph)
+
+print(EK.maxFlow("A", "Z"))
