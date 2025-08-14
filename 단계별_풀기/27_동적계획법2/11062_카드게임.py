b406efefb63b64966e2a440964cfef6b78060a49
Myung-Il <ktr040415@gmail.com>
2025-08-14 18:30:30 +0900

    골드3 - X

--- 단계별_풀기/27_동적계획법2/11062_카드게임.py
+++ 단계별_풀기/27_동적계획법2/11062_카드게임.py
@@ -1,48 +1,22 @@
 from sys import stdin
 input = lambda:stdin.readline().strip()
 
-for _ in range(int(input())):
-    n = int(input())
-    game = list(map(int, input().split()))
+T = int(input())
 
-    result = 0
+for t in range(T):
+    N = int(input())                               # 카드의 갯수
+    arr = list(map(int, input().split()))          # 카드들
+    dp = [[0 for _ in range(N)] for _ in range(N)] # dp 리스트
 
-    left, right = 0, n-1
-    while left<right:
-        remain = right-left
-        if remain<4:
-            if game[left] < game[right]:
-                result += game[right]
-                right -= 1
-            else:
-                result += game[left]
-                left -= 1
-        else:
-            if game[left] < game[right] and :
-                result += game[right]
-                right -= 1
-            else:
-                result += game[left]
-                left -= 1
+    turn = True if N%2 else False
+    # 카드의 갯수가 홀수라면 근우의 차례다
 
-'''
-3 1 4 5
-# 8
+    for size in range(N):
+        for i in range(N - size):
+            if size == 0: dp[i][i+size] = arr[i] if turn else 0
+            elif turn: dp[i][i+size] = max(dp[i+1][i+size] + arr[i], dp[i][i+size-1] + arr[i+size])
+            else: dp[i][i+size] = min(dp[i+1][i+size], dp[i][i+size-1])
+            
+        turn = not turn #차례바꿈
 
-10 12 1 1 
-# 13
-
-끝 4개만 보면 될까?
-1, 4번자리가 가장 클 경우, 그냥 더 큰거 가져가면 됨
-문제는 2, 3번자리가 가장 클 경우임
-
-50 100 ... 1 1 1
- 2 100 ... 1 1 1
-
-50을 먹어야 할지 아니면 그냥 계속 1을 먹어야할지 컴퓨터 입장에서는 알 수가 없음
-
-
-10 12 100 50 13 1 1
-
-
-'''
+    print(dp[0][N-1])
