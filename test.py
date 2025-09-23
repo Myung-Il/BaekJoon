872ddf814570838602686989b538d780679d50ec
Myung-Il <ktr040415@gmail.com>
2025-09-23 22:40:28 +0900

    플레5 - 1차
    RecursionError 뜸

--- test.py
+++ test.py
@@ -1,22 +1,2 @@
-from sys import stdin
-input = lambda:stdin.readline().strip()
-
-T = int(input())
-
-for t in range(T):
-    N = int(input())                               # 카드의 갯수
-    arr = list(map(int, input().split()))          # 카드들
-    dp = [[0 for _ in range(N)] for _ in range(N)] # dp 리스트
-
-    turn = True if N%2 else False
-    # 카드의 갯수가 홀수라면 근우의 차례다
-
-    for size in range(N):
-        for i in range(N - size):
-            if size == 0: dp[i][i+size] = arr[i] if turn else 0
-            elif turn: dp[i][i+size] = max(dp[i+1][i+size] + arr[i], dp[i][i+size-1] + arr[i+size])
-            else: dp[i][i+size] = min(dp[i+1][i+size], dp[i][i+size-1])
-            
-        turn = not turn #차례바꿈
-
-    print(dp[0][N-1])
+visit = 1<<3
+print(format(bin(visit)[2:]))
---
+++ 단계별_풀기/트리에서의동적계획법/15647_로스팅하는엠마도바리스타입니다.py
@@ -0,0 +1,41 @@
+'''
+N개의 정점을 가진 트리 형태의 농장 연결 시스템
+N : 1~N
+u, v : 서로 연결되어 있음
+d : 거리
+
+결과 : 각 특정 농장에서 다른 농장까지의 거리 합을 구해야 함
+'''
+
+from sys import stdin
+input = lambda:stdin.readline().strip()
+
+
+class Node:
+    def __init__(self, N):
+        self.num = N
+        self.edge = dict()
+    
+    def add(self, nxt, dist):
+        self.edge[nxt] = dist
+
+
+def path(idx, acnt, visit):
+    res = acnt
+
+    visit |= (1<<idx)
+    for k, v in tree[idx].edge.items():
+        if visit&(1<<k): continue
+        res += path(k, acnt+v, visit)
+    return res
+
+
+N = int(input())
+tree = [Node(idx)for idx in range(N+1)]
+
+for _ in range(N-1):
+    u, v, d = map(int,input().split())
+    tree[u].add(v, d)
+    tree[v].add(u, d)
+
+print(*[path(i, 0, 1<<(N+1))for i in range(1, N+1)], sep='\n')
