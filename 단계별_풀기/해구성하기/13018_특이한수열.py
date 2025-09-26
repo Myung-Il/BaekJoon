c3e861df7b3a8b116f3ce9608aa41313e58b2c1c
Myung-Il <ktr040415@gmail.com>
2025-09-26 18:32:19 +0900

    골드4 - O
    n과 k가 같을 때를 고려하지 않은 것과 k가 0이 될 수도 있다는 것을 간과하고 있었다

--- 단계별_풀기/해구성하기/13018_특이한수열.py
+++ 단계별_풀기/해구성하기/13018_특이한수열.py
@@ -3,7 +3,7 @@ input = lambda:stdin.readline().strip()
 
 n, k = map(int, input().split())
 
-if n==1:print("Impossible")
+if (n==1 and k>0) or n==k:print("Impossible")
 else:
     for i in range(2, n-k+1):print(i, end=' ')
     print(1, end=' ')
