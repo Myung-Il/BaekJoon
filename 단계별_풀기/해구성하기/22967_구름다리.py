4d14a7d32bafae5238911fdd124be13ed474dec7
Myung-Il <ktr040415@gmail.com>
2025-09-26 20:23:29 +0900

    골드2 - 1차
    node를 중앙값이 아닌 가장 먼저 나온 것 중 가장 많이 노드와 연결되어 있는 것으로 해두었더니 최선의 선택이 아닌 결과가 나옴

---
+++ 단계별_풀기/해구성하기/22967_구름다리.py
@@ -0,0 +1,37 @@
+from sys import stdin
+input = lambda:stdin.readline().strip()
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+if n==3:
+    print(1)
+    print(1)
+    for i in range(1, n+1):
+        for j in range(i+1, n+1):
+            if j in graph[i]:continue
+            print(i, j)
+elif n==4:
+    print(n-1)
+    print(1)
+    for i in range(1, n+1):
+        for j in range(i+1, n+1):
+            if j in graph[i]:continue
+            print(i, j)
+else: # 중앙을 찾아야 함
+    node, length = 0, 0
+    for i in range(n+1):
+        if len(graph[i]) > length:
+            node = i
+            length = len(graph[i])
+
+    print(n-length)
+    print(2)
+    for i in range(1, n+1):
+        if i in graph[node]:continue
+        print(node, i)
