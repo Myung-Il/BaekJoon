3c3e61cb69b6d2282719d54fa219a89035d60923
Myung-Il <ktr040415@gmail.com>
2025-11-11 19:29:28 +0900

    플레5 - O
    뚫려 있을 구멍의 위치를 의도적으로 정해주면 된다.

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-l = [[0, 123, 32], [100, 3, 352]]
+def a(n):
+    if n[0][0]!=10:
+        n[0][0]+=1
+        b(n)
+def b(n):
+    if n[0][0]!=10:
+        n[0][0]+=1
+        a(n)
 
-for row in l:
-    output_line = ""
-    for num in row:output_line += f"{num:>4}"
-    print(output_line)
+c = [[0]]
+a(c)
+print(c)
