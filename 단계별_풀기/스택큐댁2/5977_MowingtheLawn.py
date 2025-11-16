68dbda1851f2af6bd4199b34c8d519de882058f5
Myung-Il <ktr040415@gmail.com>
2025-11-16 23:45:05 +0900

    플레3 - X
    주석에 대한 것만 설명을 써보자면 첫번째 while문은 윈도우(k)에서 벗어난 존재를 제외하기 위한 기능
    
    최대 연속 합 구하는 코드는 아래 문자열로 주석을 따로 달아둠
    
    최적의 경우를 찾는 것은 공식에서 p[i]+max(수식)의 수식 부분을 이용해서 구하는 것인데 해당 수식 부분이 음으로 클수록 최대값에서 멀어지게 하기에 최대값만 놔두고 꺼낸다.
    같으면 어차피 최대가 아니기 때문에 꺼내면 된다.
    
    k 이후 범위 밖에 있는 슬라이딩 윈도우(swp)의 첫번째 값을 꺼낸다.
    
    그러면 이제 2가지 경우를 비교한다.
    1. 현재 숫자가 없는 경우
    2. 현재 숫자가 있는 경우
    숫자가 있다면 대신 사라져야 할 것을 찾아야한다.
    
    우리는 이미 현재 최대값을 구할 수 있다.
    굳이 범위(i-k <= j <= i)를 for으로 돌려서 구하지 않아도 이미 알고 있다.
    2번째 while문을 돌리면서 있으면 각 특정 구간에서의 최대값의 인덱스를 swp에 저장해놨기 때문이다.
    swp의 맨앞은 calculation(j)이 최대가 되는 값의 인덱스(j)를 저장해 놨다.
    이는 전체 누적합에서 제외 될 것들의 음의 합이다.
    그래서 해당 값을 구해서 (i까지의)전체 누적합과 더해버리면 된다.
    
    그럼 어떻게 최대인 것을 알고 있는냐.
    calculation(swp[-1]) <= new이 비교를 할 때, 다음 최대 추정(new)가 더 크면 현재 윈도우 안에 있는게 더 작다는 소리이니 꺼내면 된다.
    이걸 계속 비교한다.
    그러면 굳이 한번 계산 한 것을 다시 할 필요가 사라진다.
    
    기존에는 전체 n번 비교 k번으로 O(nk)만큼이 시간이 걸렸다면 지금은 전체 n번 비교 n번으로 O(2n)정도이다.
    
    기존 것은 1번 돌릴 때마다 k번을 돌리기에 nk다.
    지금은 n번 돌리면서 n을 독립적으로 따로 돌리고 있기에 2n이다.

--- 단계별_풀기/스택큐댁2/5977_MowingtheLawn.py
+++ 단계별_풀기/스택큐댁2/5977_MowingtheLawn.py
@@ -5,16 +5,27 @@ from collections import deque
 
 n, k = map(int, input().split())
 psum = [0]
-dp = []
-for num in range(n):
-    eff = int(input())
-    psum.append(eff+psum[-1])
+for num in range(n): psum.append(int(input())+psum[-1])
 
-for i in range(k+1):dp.append(psum[i])
-for i in range(k+1, n+1):
-    c1 = dp[i-1]
-    c2 = psum[i]+max([dp[j-1]-psum[j]for j in range(i-k, i)])
-    dp.append(max(c1, c2))
+dp = [0]*(n+1)
+swp = deque([0])
+
+def calculation(j):
+    if j==0:return 0
+    else: return dp[j-1]-psum[j]
+
+for i in range(1, n+1):
+    # 슬라이딩 범위를 벗어난 요소를 제거
+    while swp and swp[0] < i-k:swp.popleft()
+
+    # 최대 연속 합 구하기
+    dp[i] = max(dp[i-1], psum[i]+calculation(swp[0]))
+
+    # 연속 구간에 대한 다른 경우 중 최적 찾기
+    new = calculation(i)
+    while swp and calculation(swp[-1]) <= new:swp.pop()
+
+    swp.append(i)
 
 print(dp[-1])
 
@@ -25,7 +36,7 @@ print(dp[-1])
 
 - 정의
 1. psum : 누적합
-2. dp : k개의 비연속 누적합
+2. dp : k개의 연속 누적합
 
 - 점화식 (1 <= flag <= k)
 1. 현재 소를 선택하지 않았을 때 : dp[i-1]
