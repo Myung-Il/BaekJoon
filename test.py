da069f379a137f430d78acaf063fce00fe5e818f
Myung-Il <ktr040415@gmail.com>
2025-11-16 21:46:11 +0900

    플레3 - 2차
    내가 풀어낸 방식은 그리디였다.
    
    지금은 시간초과다.
    O(k)인 부분을 O(1)로 줄여야한다.

--- test.py
+++ test.py
@@ -3,38 +3,59 @@ input = lambda:stdin.readline().strip()
 
 import heapq
 
+class Node:
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.chain = 0
+
+    def leftCheck(self):
+        if self.left:return self.left.chain
+        else: return 0
+
+    def rightCheck(self):
+        if self.right:return self.right.chain
+        else: return 0
+
+    
+    def chainCheck(self):
+        cond = self.leftCheck()+1+self.rightCheck()
+        if cond<=k:return cond
+        else: return 0
+
+
 n, k = map(int, input().split())
 cows = []
 for num in range(n):
     eff = int(input())
     heapq.heappush(cows, (-eff, num+1))
 
+linkedlist = [Node()for _ in range(n+1)]
+for num in range(1, n):
+    linkedlist[num].right = linkedlist[num+1]
+    linkedlist[num+1].left = linkedlist[num]
 
-class Node:
-    def __init__(self, l, r, n):
-        self.left = l
-        self.right = r
-        self.chain = n
-        
-
-linkedlist = [None]*(n+2)
-for num in range(1, n+1):
-    linkedlist[num] = Node(linkedlist[num-1], linkedlist[num+1], 0)
-
-res = 0
+result = 0
 for _ in range(n):
     eff, num = heapq.heappop(cows)
 
     now = linkedlist[num]
-    if not now.chain:
-        chain = now.left.chain+1+now.right.chain
-        if chain<k:
-            now.chain = chain
-            now.left.chain = chain
-            now.right.chain = chain
-            res -= eff
+    # continuous cows count
+    ccc = now.chainCheck()
+    now.chain = ccc
 
-print(res)
+    if not ccc:continue
+    else:result -= eff
+
+    if now.leftCheck():
+        now.left.right = now.right
+        now.left.chain = ccc
+
+    if now.rightCheck():
+        now.right.left = now.left
+        now.right.chain = ccc
+
+print(result)
 
 
 
@@ -47,5 +68,6 @@ print(res)
 2. 우선순위 큐를 이용해서 극한의 효율을 자랑하는 소를 순서대로 가져옴
 3. 소에게 연속된 항목에 포함되는지 유효성 검사
 4. 효율성으로 먼저 받고 연속 항목 어디에 포함되는지 확인
+
 
 '''
--- 단계별_풀기/스택큐댁2/5977_MowingtheLawn.py
+++ 단계별_풀기/스택큐댁2/5977_MowingtheLawn.py
@@ -1,72 +1,43 @@
 from sys import stdin
 input = lambda:stdin.readline().strip()
 
-import heapq
-
-class Node:
-    def __init__(self):
-        self.left = None
-        self.right = None
-        self.chain = 0
-
-    def leftCheck(self):
-        if self.left:return self.left.chain
-        else: return 0
-
-    def rightCheck(self):
-        if self.right:return self.right.chain
-        else: return 0
-
-    
-    def chainCheck(self):
-        cond = self.leftCheck()+1+self.rightCheck()
-        if cond<=k:return cond
-        else: return 0
-
+from collections import deque
 
 n, k = map(int, input().split())
-cows = []
+psum = [0]
+dp = []
 for num in range(n):
     eff = int(input())
-    heapq.heappush(cows, (-eff, num+1))
+    psum.append(eff+psum[-1])
 
-linkedlist = [Node()for _ in range(n+1)]
-for num in range(1, n):
-    linkedlist[num].right = linkedlist[num+1]
-    linkedlist[num+1].left = linkedlist[num]
+for i in range(k+1):dp.append(psum[i])
+for i in range(k+1, n+1):
+    c1 = dp[i-1]
+    c2 = psum[i]+max([dp[j-1]-psum[j]for j in range(i-k, i)])
+    dp.append(max(c1, c2))
 
-result = 0
-for _ in range(n):
-    eff, num = heapq.heappop(cows)
-
-    now = linkedlist[num]
-    # continuous cows count
-    ccc = now.chainCheck()
-    now.chain = ccc
-
-    if not ccc:continue
-    else:result -= eff
-
-    if now.leftCheck():
-        now.left.right = now.right
-        now.left.chain = ccc
-
-    if now.rightCheck():
-        now.right.left = now.left
-        now.right.chain = ccc
-
-print(result)
+print(dp[-1])
 
 
-
+...
 '''
-힌트 1. 소들끼리는 서로의 정보를 알고 있다
-힌트 2. 덱을 사용한다
+[ 동적 계획법 ]
 
-추론
-1. 연결 리스트로 덱을 구현
-2. 우선순위 큐를 이용해서 극한의 효율을 자랑하는 소를 순서대로 가져옴
-3. 소에게 연속된 항목에 포함되는지 유효성 검사
-4. 효율성으로 먼저 받고 연속 항목 어디에 포함되는지 확인
+- 정의
+1. psum : 누적합
+2. dp : k개의 비연속 누적합
+
+- 점화식 (1 <= flag <= k)
+1. 현재 소를 선택하지 않았을 때 : dp[i-1]
+2. 선택 했을 때 : max(psum[i]-psum[i-flag] + dp[i-flag-1]) : psum[i] + max(dp[j-1]-psum[j])
+    - psum[i]-psum[i] : 특정 구간의 최대합
+    - dp[i-flag-1] : 특정 구간 이전의 최대합
+    - : i-flag = j
+        >>> (1 <= i-j <= k) : (i-k <= j <= i-1) : 쉽게 보기
+        >>> max(psum[i]-psum[j] + dp[j-1]) : psum[i] + max(dp[j-1]-psum[j])
+3. 두 경우를 한꺼번에 볼 때 (단, (i-k <= j <= i)) : psum[i] + max(dp[j-1]-psum[j])
+
+- 덱 이용 (슬라이딩 윈도우)
+1. 지나간 위치는 dp에 저장이 되어 있음
 
 '''
